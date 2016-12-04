c95fc88e445e6f5b67d94d7f71401e570cb32f4c
Amit Kumar Gupta <hriks@users.noreply.github.com>
2016-12-04 23:45:53 +0530

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
-API_KEY = "778cde9bf763afec1797d5e8233a5812"
+API_KEY = "XXXXXXXXXXXXXXXXXXXXXX"
 RESOURCE_ID = "0a076478-3fd3-4e2c-b2d2-581876f56d77"
 PINCODE = 713338
 URL = "https://data.gov.in/api/datastore/resource.json"
